509050f5e97b8e49dbb808acd0d8f9690c7cbb92
Ari Ayvazyan <ariay@live.at>
2015-04-19 22:35:40 +0200

    comments comments comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,9 +8,12 @@ import java.io.IOException;
 //http://www.mkyong.com/webservices/jax-ws/jax-ws-hello-world-example/
 public class Main {
     public static void main(String[] args) throws IOException {
-            new JettyEmbeddedRunner().startServer(8081);
-            System.out.println("Started Servlet/s");
-            SOAPublisher.publish(); //8082
-            new SOAClient("localhost", 8082, "SOA");
+        //reachable at localhost:8081
+        // Also starts a rest service at localhost/rest
+        new JettyEmbeddedRunner().startServer(8081);
+
+        System.out.println("Started Servlet/s");
+        SOAPublisher.publish(); //8082
+        new SOAClient("localhost", 8082, "SOA");
     }
 }
